d5620dc99195bf5edc9d65b17f56766f30b2eb7c
OlivierThiebaux <olivier.thiebaux@live.fr>
2024-11-04 18:08:31 +0000

    je me suis perdu dans l'arbre

--- math.go
+++ math.go
@@ -2,14 +2,14 @@ package main
 
 import "errors"
 
-func multiplication (a int, b int) int {
-	return	a * b
+func multiplication(a int, b int) int {
+	return a * b
 }
-func division (a float64, b float64) (float64, error) {
-	if b == 0 { 
+func division(a float64, b float64) (float64, error) {
+	if b == 0 {
 		err := errors.New("division par 0 impossible")
 		return 0, err
-	}	
+	}
 	return (a / b), nil
 
 }
