91c7a9280c26ec434c00a5e568ec4f87054d45f4
OlivierThiebaux <olivier.thiebaux@live.fr>
2024-11-04 16:35:39 +0000

    aie-aie

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
-import "fmt"
+import "log"
 
 func ajouter(a int, b int) int {
 	return a + b
 }
+
 var somme int
 
 func main() {
-	fmt.Println(`Hello world !!!`)
-	somme := ajouter(3 , 4)
-	fmt.Printf("la somme de 3 et 4 est : %v\n" , somme, "coucou", "arg")
+	log.Println(`Hello world !!!`)
+	somme := ajouter(3, 4)
+	log.Printf("la somme de 3 et 4 est : %v\n", somme)
+	log.Printf("la multiplication de 3 et 4: %v\n", multiplication(3, 4))
+	resultat, err := division(3, 0)
+	if err != nil {
+		log.Fatalf("alerte, division par zéro : %v", err)
+	}
+	log.Printf("la division de 3 et 4: %v", resultat)
+
 }
