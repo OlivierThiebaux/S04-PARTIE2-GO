0eacf93f93b8d7e6e72d9ff03fd5b5b61a8643cf
OlivierThiebaux <olivier.thiebaux@live.fr>
2024-11-05 08:46:40 +0000

    P2

--- main.go
+++ main.go
@@ -7,7 +7,22 @@ import ("log"
 func main() {
 	//var task string
 	var reponse string
-	log.Printf("Ajouter une tâche: ")
+	log.Printf("Voulez vous ajouter une tâche (O/N): ")
 	fmt.Scanln(reponse)
 	log.Printf("reponse: %v", reponse)
-}
+	switch reponse {
+	case "y", "o","yes","oui":
+		scan := bufio.NewScanner(os.Stdin)
+        scan.Split(bufio.ScanLines)
+        log.Println("Reponse oui")
+        log.Printf("Saisir la description de la tâche")
+        scan.Scan()
+        input := scan.Text()
+        task = input
+        log.Printf("Valeur saisie : %v", input)
+		
+	case "n","non","no":
+		log.Println ("Réponse non")
+	default 
+	}
+	}
