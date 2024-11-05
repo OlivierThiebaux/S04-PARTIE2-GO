3b06270974d23f6f36aad23b33473cf61f26325a
OlivierThiebaux <olivier.thiebaux@live.fr>
2024-11-05 09:44:12 +0000

    P4

--- main.go
+++ main.go
@@ -1,28 +1,33 @@
 package main
 
-import ("log"
-		"fmt"
-	)
-
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+var task string
 func main() {
-	//var task string
+	
 	var reponse string
 	log.Printf("Voulez vous ajouter une tâche (O/N): ")
 	fmt.Scanln(reponse)
 	log.Printf("reponse: %v", reponse)
 	switch reponse {
-	case "y", "o","yes","oui":
+	case "y", "o", "yes", "oui":
 		scan := bufio.NewScanner(os.Stdin)
-        scan.Split(bufio.ScanLines)
-        log.Println("Reponse oui")
-        log.Printf("Saisir la description de la tâche")
-        scan.Scan()
-        input := scan.Text()
-        task = input
-        log.Printf("Valeur saisie : %v", input)
-		
-	case "n","non","no":
-		log.Println ("Réponse non")
-	default 
+		scan.Split(bufio.ScanLines)
+		log.Println("Reponse oui")
+		log.Printf("Saisir la description de la tâche")
+		scan.Scan()
+		input := scan.Text()
+		task = input
+		log.Printf("Valeur saisie : %v", input)
+
+	case "n", "non", "no":
+		log.Println("Réponse non")
+	default:
+		log.Printf("réponse inconnu : %v", reponse)
 	}
-	}
+
+}
